0bfd493b7ea48da74f31ea8ef4029093bb452e4f
Trevor Richardson <Tjrich336@gmail.com>
2023-05-05 10:49:20 -0400

    Removed instagram link and social icon, removed 'services' section.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -31,12 +31,6 @@ const Header = () => {
                         </li>
 
                         <li className="nav__item">
-                            <a href="#services" className="nav__link">
-                                <i className="uil uil-briefcase-alt nav__icon"></i> Services
-                            </a>
-                        </li>
-
-                        <li className="nav__item">
                             <a href="#portfolio" className="nav__link">
                                 <i className="uil uil-scenery nav__icon"></i> Portfolio
                             </a>
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,11 +3,6 @@ import React from "react";
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://www.instagram.com/tjrich_34/" 
-            className="home__social-icon" target="_blank">
-                <i class="uil uil-instagram"></i>
-            </a>
-
             <a href="https://www.linkedin.com/in/trevv/" 
             className="home__social-icon" target="_blank">
                 <i class="uil uil-linkedin-alt"></i>
