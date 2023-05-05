d08f1cc7b80bd07cd43bcd394bd2f4a7509b1dd4
Trevor Richardson <Tjrich336@gmail.com>
2023-05-05 09:49:38 -0400

    Creation of React Personal Portfolio Website, Structure and implemenation beginning. Completion: Pending...

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+    <h3 className="skills__title">Backend Developer</h3>
+
+    <div className="skills__box">
+        <div className="skills__group">
+            <div className="skills__data">
+            <i class='bx bx-badge-check'></i>
+
+            <div>
+                <h3 className="skills__name">Java</h3>
+                <span className="skills__level">Advanced</span>
+            </div>
+            </div>
+
+            <div className="skills__data">
+            <i class='bx bx-badge-check'></i>
+
+            <div>
+                <h3 className="skills__name">SQL / MySQL</h3>
+                <span className="skills__level">Intermediate</span>
+            </div>
+            </div>
+
+            <div className="skills__data">
+            <i class='bx bx-badge-check'></i>
+
+            <div>
+                <h3 className="skills__name">Python</h3>
+                <span className="skills__level">Beginner</span>
+            </div>
+            </div>
+
+            <div className="skills__data">
+            <i class='bx bx-badge-check'></i>
+
+            <div>
+                <h3 className="skills__name">Node Js</h3>
+                <span className="skills__level">Beginner</span>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+  );
+};
+
+export default Backend;
