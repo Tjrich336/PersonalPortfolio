27ec1d7151bd1e1329028427329317f137a2f7ef
Trevor Richardson <Tjrich336@gmail.com>
2023-07-27 18:05:59 -0400

    Connected email.js, basic styling.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,36 +1,60 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import emailjs from 'emailjs-com';
-import "./contact.css";
+import './contact.css';
 
 const Contact = () => {
-
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_x60sgmp', 'template_jjtlbks', form.current, 'eSHOuOTrbECcYi0vQ')
+    const serviceID = 'service_9apk1rd';
+    const templateID = 'template_jjtlbks';
+    const userID = 'eSHOuOTrbECcYi0vQ';
+
+    emailjs
+      .sendForm(serviceID, templateID, form.current, userID)
       .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
+        console.log(result.text);
+      })
+      .catch((error) => {
+        console.log(error.text);
       });
-      e.target.reset()
+
+    e.target.reset();
   };
 
   return (
-    <section className="contact section" id='contact'>
+    <section className="contact section" id="contact">
       <h2 className="section__title">Contact</h2>
       <span className="section__subtitle">Information</span>
 
       <div>
-        <h2>Reach Out!</h2>
+        <h2 className="call-to-action">Reach Out!</h2>
         <form ref={form} onSubmit={sendEmail} className="form">
-            <input type="text" className="userName" placeholder="Full Name" name="user__name" required/>
-            <input type="email" className="userEmail" placeholder="Email" name="user__email" required/>
-            <input type="message" className="userMessage"placeholder="Message" name="user__message" required/>
-            <button type="submit" className="send__button">Send Message</button>
-
+          <input
+            type="text"
+            className="userName"
+            placeholder="Full Name"
+            name="user__name"
+            required
+          />
+          <input
+            type="email"
+            className="userEmail"
+            placeholder="Email"
+            name="user__email"
+            required
+          />
+          <textarea
+            className="userMessage"
+            placeholder="Message"
+            name="user__message"
+            required
+          ></textarea>
+          <button type="submit" className="send__button">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
