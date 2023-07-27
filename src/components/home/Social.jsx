bd9bde1504288ac4628233bbab9f42e3b8f1cb9d
Trevor Richardson <Tjrich336@gmail.com>
2023-07-27 16:32:13 -0400

    Updated social.jsx configuration settings.

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 
 const Social = () => {
-    return (
-        <div className="home__social">
-            <a href="https://www.linkedin.com/in/trevv/" 
-            className="home__social-icon" target="_blank">
-                <i class="uil uil-linkedin-alt"></i>
-            </a>
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/trevv/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
 
-            <a href="https://github.com/Tjrich336" 
-            className="home__social-icon" target="_blank">
-                <i class="uil uil-github-alt"></i>
-            </a>
-        </div>
-    );
+      <a
+        href="https://github.com/Tjrich336"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
 };
 
-export default Social
-
-
-
-
-
+export default Social;
