f6a7415c5cf51cbdf05a66633c46d94415a2c3cf
Trevor Richardson <Tjrich336@gmail.com>
2023-07-30 17:19:49 -0400

    Added breakpoints: Large devices (476px - 1000px)

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,21 +6,21 @@ const Info = () => {
             <div className="about__box">
             <i class="uil uil-trophy about__icon"></i>
 
-                <h3 className="about__title">Experience</h3>
+                <h3 className="about__title2">Experience</h3>
                 <span className="about__subtitle">B.S Computer Science</span>
             </div>
 
             <div className="about__box">
             <i class="uil uil-briefcase-alt about__icon"></i>
 
-                <h3 className="about__title">Completed</h3>
+                <h3 className="about__title2">Completed</h3>
                 <span className="about__subtitle">5+ Projects</span>
             </div>
 
             <div className="about__box">
             <i class="uil uil-envelope-question about__icon"></i>
 
-                <h3 className="about__title">Support</h3>
+                <h3 className="about__title2">Support</h3>
                 <span className="about__subtitle">Online 24/7</span>
             </div>
         </div>
